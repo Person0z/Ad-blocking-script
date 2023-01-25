9608de63b049ddc0c10cc24ded150c86dd246c55
Person0z <80363995+Person0z@users.noreply.github.com>
2023-01-24 21:39:57 -0600

    Update adblock-f.sh

--- adblock-f.sh
+++ adblock-f.sh
@@ -1,7 +1,24 @@
-#!/bin/sh
-if [[ -d /etc/adblock-scripts ]]; 
-then
-    cd /etc/adblock-scripts; curl https://hosts.oisd.nl/ > hosts; cd /etc/adblock-scripts; mv hosts /etc; cd
-else 
-    cd /etc; mkdir adblock-scripts; cd /etc/adblock-scripts; curl https://hosts.oisd.nl/ > hosts; cd /etc/adblock-scripts; mv hosts /etc; cd
+#!/bin/bash
+
+if [ ! -d "/etc/adblock-scripts" ]; then
+  mkdir /etc/adblock-scripts
+fi
+
+echo "This will update your current hosts file to update/add the ad blocking. Do you want to continue (yes/no)?"
+read user_input
+
+if [ "$user_input" == "yes" ]; then
+  echo "Thank you for confirming, beginning now..."
+  cd /etc/adblock-scripts
+
+  curl https://hosts.oisd.nl/ > hosts
+
+  mv hosts /etc
+
+  cd
+  echo "Finished"
+elif [ "$user_input" == "no" ]; then
+  echo "Sorry to hear that, Have a good day!"
+else
+  echo "Invalid input, please try again"
 fi
